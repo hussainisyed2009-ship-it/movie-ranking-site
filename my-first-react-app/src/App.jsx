ad3fd140343266866072698fe0572da3f3b9bcc4
hussainisyed2009-ship-it <hussainisyed2009@gmail.com>
2026-01-20 22:20:41 -0500

    .

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <main>
+    <main className="bg-[url('./background.png')] bg-cover bg-center bg-fixed min-h-screen">
       <div className="pattern"/>
 
       <div className='wrapper'>
         <header>
+          
+          <img src="./recommendations.png" alt="Hero Banner"/>
           <h1>Find <span className='text-gradient'>Movies</span> You'll enjoy Without The Hastle</h1>
           </header>
 
           <p>Search</p>
-
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   )
